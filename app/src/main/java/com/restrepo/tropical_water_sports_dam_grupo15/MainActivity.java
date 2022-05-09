ed3851849eefbbe5dae16d86a7be155d05e01128
Jorgerestrepo1982 <105014127+Jorgerestrepo1982@users.noreply.github.com>
2022-05-09 11:38:31 +0200

    Initial commit

---
+++ app/src/main/java/com/restrepo/tropical_water_sports_dam_grupo15/MainActivity.java
@@ -0,0 +1,14 @@
+package com.restrepo.tropical_water_sports_dam_grupo15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
